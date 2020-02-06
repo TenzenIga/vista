945518222d27bfb080c3449eb11406d96b95dc31
TenzenIga <arhan-93@list.ru>
2020-02-06 20:02:57 +0300

    Updates

---
+++ precache-manifest.6668c22b46ce1edc513c1f3fcf2172da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8aafe2ea412bdde7ae1578fc45475d",
+    "url": "/vista/index.html"
+  },
+  {
+    "revision": "c77635ccc6998df19686",
+    "url": "/vista/static/css/main.1612a597.chunk.css"
+  },
+  {
+    "revision": "67f8dc3f108e7db5cf56",
+    "url": "/vista/static/js/2.86aaca49.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/vista/static/js/2.86aaca49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77635ccc6998df19686",
+    "url": "/vista/static/js/main.5c9bf1db.chunk.js"
+  },
+  {
+    "revision": "d762270d766f9c09345b",
+    "url": "/vista/static/js/runtime-main.ad1c0760.js"
+  }
+]);
